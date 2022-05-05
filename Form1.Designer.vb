613c732da96c0574dd1c996fb2d69d78c37147e7
SuperTractopelle <104142868+SuperTractopelle@users.noreply.github.com>
2022-05-05 16:10:30 +0200

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.BtOui = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BtNon = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'BtOui
+        '
+        Me.BtOui.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtOui.Location = New System.Drawing.Point(7, 34)
+        Me.BtOui.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.BtOui.Name = "BtOui"
+        Me.BtOui.Size = New System.Drawing.Size(300, 134)
+        Me.BtOui.TabIndex = 0
+        Me.BtOui.TabStop = False
+        Me.BtOui.Text = "OUI JE SUIS UNE FEIGNASSE !"
+        Me.BtOui.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(38, 9)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(216, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Es-tu une grosse feignasse ?"
+        '
+        'BtNon
+        '
+        Me.BtNon.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtNon.Location = New System.Drawing.Point(7, 177)
+        Me.BtNon.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.BtNon.Name = "BtNon"
+        Me.BtNon.Size = New System.Drawing.Size(300, 134)
+        Me.BtNon.TabIndex = 2
+        Me.BtNon.TabStop = False
+        Me.BtNon.Text = "NON JE TRAVAILLE ÉNORMEMENT !"
+        Me.BtNon.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 250
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(315, 323)
+        Me.Controls.Add(Me.BtNon)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtOui)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents BtOui As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents BtNon As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+
+    Dim QuestionValidation As Boolean = False
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        If Not Validate() Then e.Cancel = True 'on empeche la fermeture si ce n'est pas validé
+    End Sub
+
+    Private Sub Form1_Shown(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Shown
+        Me.TopMost = True 'on définit la fenêtre en par dessus tout
+        Me.Activate() 'on définit que la fenêtre est active
+        Timer1.Enabled = True 'on démarre le timer
+        Cursor.Clip = BtOui.RectangleToScreen(BtOui.ClientRectangle) 'on bloque la souris dans la case oui
+    End Sub
+
+    Private Sub Btoui_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtOui.Click
+        Timer1.Enabled = False
+        Cursor.Clip = Nothing 'on libère la souris
+        QuestionValidation = True 'on autorise la fermeture
+        MsgBox("Ta réponse est enregistrée, nous le savions tous que tu étais une grosse feignasse, maintenant tu l'avoues c'est encore mieux", MsgBoxStyle.Information, "Question") 'on affiche le message
+        Me.Close() 'on ferme l'application
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Cursor.Clip = BtOui.RectangleToScreen(BtOui.ClientRectangle) 'on bloque la souris dans le bouton, on le refait toutes les 250msec pour éviter qu'elle soit libérer avec un ctrl alt suppr
+    End Sub
+
+End Class
